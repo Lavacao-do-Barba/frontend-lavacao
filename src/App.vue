fe82982c4c212afe5e1cea06f191f957384e4aae
vitormsantanaa <vitormanoelssz1704@gmail.com>
2026-08-25 22:13:31 -0300

    FEAT: tela de login com autenticacao real e protecao de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import AppLayout from './layouts/AppLayout.vue'
+
+const route = useRoute()
 </script>
 
 <template>
-  <AppLayout />
+  <AppLayout v-if="route.meta.requiresAuth" />
+  <router-view v-else />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/Login.vue'
+import { isAuthenticated } from '../services/auth'
 
 const routes = [
-  { path: '/', name: 'dashboard', component: Dashboard },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/', name: 'dashboard', component: Dashboard, meta: { requiresAuth: true } },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    return { name: 'login' }
+  }
 })
 
 export default router
